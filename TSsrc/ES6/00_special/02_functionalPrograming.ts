38bcf5419bd345777c8e9aea5527b419710cfbe4
June Han <codenamenadja@gmail.com>
2018-12-13 03:49:08 +0900

    0.2 /done

--- TSsrc/ES6/00_special/02_functionalPrograming.ts
+++ TSsrc/ES6/00_special/02_functionalPrograming.ts
@@ -1,2 +1,53 @@
-console.log("--Functional programing in Typescript---");
-console.log("이 챕터에서는 함수형 프로그래밍을 객체지향적 언어인 타입스크립트의 객체지향적 특성에서 어떻게 적용할지에 대한 준비를 해보록한다.");
+import * as _ from "lodash";
+
+interface IuserDataPreset {
+    id: string;
+    name: string;
+    age: number;
+}
+type CallbackFuncType = (callback: any, argA: IuserDataPreset[]) => IuserDataPreset[];
+const userList: IuserDataPreset[] = [
+    { id: "A", name: "aaa", age: 12 },
+    { id: "B", name: "bbb", age: 42 },
+    { id: "C", name: "ccc", age: 23 },
+    { id: "D", name: "ddd", age: 33 },
+    { id: "E", name: "eee", age: 51 },
+    { id: "F", name: "fff", age: 25 },
+    { id: "G", name: "ggg", age: 17 },
+    { id: "H", name: "hhh", age: 43 },
+    { id: "I", name: "iii", age: 35 },
+];
+
+const getFilteredData: CallbackFuncType = (callback, dataList) => {
+    const result = dataList.filter((data) => callback(data));
+    return result;
+}
+const dataA = getFilteredData((data: IuserDataPreset) => data.age > 30, userList);
+console.log(dataA);
+
+interface Isingletone {
+    name?: string;
+    date: Date;
+}
+class Sample {
+    public static instanceObj: Isingletone;
+    private static _init_(inputData?: string): Isingletone{
+        return {
+            date: new Date(),
+            name: inputData ? inputData : "no name init",
+        };
+    }
+    constructor(public name?: string) {
+        if (!Sample.instanceObj) {
+            Sample.instanceObj = Sample._init_(name);
+        }
+        return Sample.instanceObj;
+    }
+}
+const sample0 = new Sample();
+const sample1 = new Sample("key");
+const sample2 = new Sample();
+
+console.log(sample1);
+console.log(sample2 === sample1);
+console.log(sample2 === sample0);
